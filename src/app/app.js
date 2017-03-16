350e9ddb4dd61b50a2bff89b5b8ff4e7ec481c83
Aukai Tirrell <mtirrell@hawaii.edu>
2017-03-15 20:41:40 -1000

    added basic folder structure

--- src/app/app.js
+++ src/app/app.js
@@ -5,16 +5,9 @@ import '../style/app.css';
 let app = () => {
   return {
     template: require('./app.html'),
-    controller: 'AppCtrl',
     controllerAs: 'app'
-  }
+  };
 };
-
-class AppCtrl {
-  constructor() {
-    this.url = 'https://github.com/preboot/angular-webpack';
-  }
-}
 
 const MODULE_NAME = 'app';
 
