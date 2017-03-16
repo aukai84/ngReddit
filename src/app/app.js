c2753681c18e0d000640da713106c5291c060b4e
Aukai Tirrell <mtirrell@hawaii.edu>
2017-03-15 22:04:03 -1000

    added default controller page

--- src/app/app.js
+++ src/app/app.js
@@ -1,18 +1,31 @@
 import angular from 'angular';
+import {DefaultState, DefaultCtrl} from './default';
+import * as uiRouter from 'angular-ui-router';
+
+console.log(DefaultState.controller)
+console.log(DefaultCtrl)
 
 import '../style/app.css';
 
 let app = () => {
   return {
     template: require('./app.html'),
-    controllerAs: 'app'
   };
 };
 
 const MODULE_NAME = 'app';
 
-angular.module(MODULE_NAME, [])
-  .directive('app', app)
-  .controller('AppCtrl', AppCtrl);
+angular.module(MODULE_NAME, ['ui.router'])
+    .config(($stateProvider) => {
+        $stateProvider
+            .state(DefaultState.name, DefaultState);
+    })
+    .directive('app', app)
+
+    .run(($state) => {
+        $state.go('default');
+    })
+    .controller(DefaultState.controller, DefaultCtrl);
+
 
 export default MODULE_NAME;
